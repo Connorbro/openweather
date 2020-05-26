7cbf3d9ecd55b9e3aa47233c57b5a98a3a6a918b
connor <vbrookesy@hotmail.co.uk>
2020-05-26 17:01:07 +0100

    open weather

---
+++ index.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const APPID = '224566a483405adb54b0ecf2dbbce14a';
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${APPID}`;
+
+const getWeather = async()=> {
+
+    let data = await fetch(url)
+    let jsonData = await fetch(url)
+    //fs.writeFileSync('data.json', data)
+    console.log(await data.json())
+}
+
+getWeather()
